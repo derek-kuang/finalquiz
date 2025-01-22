6e44fef9abe36575a12e5c7300b8fde47d94c9e5
derek-kuang <derekk7966@gmail.com>
2025-01-21 18:43:06 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -16,6 +16,31 @@ if ('serviceWorker' in navigator) {
   }  
 
 
+// handle install prompt
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+}); 
+
+
 async function fetchEasy(){
     const response = await fetch(questionURL) //+ "easy")
     const data = await response.json()
